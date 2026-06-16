c232a7a956e3df067ad847cf49e5398cc34c7e3f
Marco Giacalone <mgiacalo@gmail.com>
2026-06-16 13:22:38 +0200

    Fetch repos.js

---
+++ www/repos.js
@@ -0,0 +1,7 @@
+var REPOS_CONFIG = [
+  { id: "O2DPG",     full: "AliceO2Group/O2DPG"    },
+  { id: "AliceO2",   full: "AliceO2Group/AliceO2"  },
+  { id: "alidist",   full: "alisw/alidist"          },
+  { id: "EPOS4",   full: "alisw/EPOS4"          },
+  { id: "O2Physics", full: "AliceO2Group/O2Physics" }
+];
