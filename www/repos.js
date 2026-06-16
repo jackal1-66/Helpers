d109b7280e2432c86e9f13156631b5faf7beebb5
Marco Giacalone <mgiacalo@gmail.com>
2026-06-16 13:27:23 +0200

    Set defaults

--- www/repos.js
+++ www/repos.js
@@ -2,6 +2,5 @@ var REPOS_CONFIG = [
   { id: "O2DPG",     full: "AliceO2Group/O2DPG"    },
   { id: "AliceO2",   full: "AliceO2Group/AliceO2"  },
   { id: "alidist",   full: "alisw/alidist"          },
-  { id: "EPOS4",   full: "alisw/EPOS4"          },
   { id: "O2Physics", full: "AliceO2Group/O2Physics" }
 ];
